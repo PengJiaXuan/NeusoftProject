5ab79b37dc46c5ad100b500663ac90826d25b0a0
alex <1813056052@qq.com>
2024-07-09 11:58:02 +0800

    change1

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,49 @@ import SongList from './views/SongList.vue';
 
 <style>
 /* 添加一些基本样式 */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+a {
+  color: #42b983;
+}
+
+h1, h2, h3 {
+  font-weight: normal;
+  margin: 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+button {
+  background-color: #42b983;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 4px;
+}
+
+button:hover {
+  background-color: #36a173;
+}
 </style>
