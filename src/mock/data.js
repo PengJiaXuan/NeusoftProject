7a6247f8f9d26027f7f6627a6278f9ee425c3d05
alex <1813056052@qq.com>
2024-07-09 10:39:35 +0800

    Add audio files to Git LFS

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,6 +1,5 @@
 export const songs = [
-    { id: 1, name: 'Song 1', url: '/song1.mp3' },
-    { id: 2, name: 'Song 2', url: '/song2.mp3' },
-    { id: 3, name: 'Song 3', url: '/song3.mp3' },
-  ];
-  
+  { id: 1, name: 'Alivvve - All We Have', url: '/Alivvve - All We Have.mp3' },
+  { id: 2, name: 'Bubble Tea and Cigarettes - Room 907', url: '/Bubble Tea and Cigarettes - Room 907.mp3' },
+  { id: 3, name: 'hozuki - snowfield', url: '/hozuki - snowfield.mp3' },
+];
