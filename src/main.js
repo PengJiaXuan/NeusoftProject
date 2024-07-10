8b0f37ba65c4234a73f6159bfc9602fe9bb2c0ec
alex <1813056052@qq.com>
2024-07-10 09:03:41 +0800

    m

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import pinia from './store';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const app = createApp(App);
-app.use(pinia);
-app.mount('#app');
+createApp(App).mount('#app')
