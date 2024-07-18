dc0a96ad84acd7c6a2d6b9c66355ab6c7d8be131
alex <1813056052@qq.com>
2024-07-19 07:36:44 +0800

    change agian

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
-import store from './stores/yourStore'; // Ensure this path is correct
+import store from './stores/yourStore'; 
 import './styles/style.css'; 
 import { Buffer } from 'buffer';
 
@@ -9,6 +9,6 @@ window.Buffer = Buffer;
 
 const app = createApp(App);
 
-app.use(store); // Use Vuex store
+app.use(store); 
 
 app.mount('#app');
